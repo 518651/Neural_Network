f13ff323beb53519eff9e3061a363fa760eb919e
518651 <69994181+518651@users.noreply.github.com>
2022-12-24 22:03:59 +0800

    Add files via upload

---
+++ Paddle-BTC/outdata.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+import warnings
+
+warnings.filterwarnings("ignore")
+
+proxy = {'https': 'http://localhost:10809'}
+
+url = "https://web-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical"
+param = {"convert": "USD", "slug": "bitcoin", "time_end": "1601510400", "time_start": "1367107200"}
+content = requests.get(url=url, params=param, proxies=proxy, verify=False).json()
+df = pd.json_normalize(content['data']['quotes'])
+
+# 提取和重命名重要变量
+df['Date'] = pd.to_datetime(df['quote.USD.timestamp']).dt.tz_localize(None)  # 继承子级嵌套Data
+df['Low'] = df['quote.USD.low']  # 开盘USD价格
+df['High'] = df['quote.USD.high']  # USD最高价格
+df['Open'] = df['quote.USD.open']  # USD最低价格
+df['Close'] = df['quote.USD.close']  # 闭盘USD价格
+df['Volume'] = df['quote.USD.volume']  # 保留参数
+
+# 删除原始列和冗余列
+df = df.drop(
+    columns=['time_open', 'time_close', 'time_high', 'time_low', 'quote.USD.low', 'quote.USD.high', 'quote.USD.open',
+             'quote.USD.close', 'quote.USD.volume', 'quote.USD.market_cap', 'quote.USD.timestamp'])
+
+#  导出数据
+outdata = df
+outdata.to_excel('BTC-COIN-DATA.xlsx', index=False)
